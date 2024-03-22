e5216f95d6ce4ebd9c4bfdcb01442d3693b32005
RubyJoe26 <ruby262001@gmail.com>
2024-03-22 07:47:30 +0530

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import HomePageView
+from .views import AboutPageView
+
+
+urlpatterns = [
+    path('',HomePageView.as_view(),name= 'home'),
+    path('about/',AboutPageView.as_view(), name = 'about'), #new deployment
+]
+
